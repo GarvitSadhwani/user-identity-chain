3056682d35b776e782f08c17415f2b086f4e2c76
Garvit Sadhwani <garvit.sadh@gmail.com>
2024-06-01 18:27:52 +0530

    error handling

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ app.use(bodyParser.json());
 app.post('/identity',handleContact);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hey there, this service is created for bitespeed backend task by Garvit. Give it a try! Send a post request to /identity with {email: \'emailid\', phoneNumer: \'phno\'}');
+});
+
+app.get('/*', (req, res) => {
+  res.send('Sorry, This endpoint doesn\'t exist :)');
 });
 
 app.listen(port, () => {
